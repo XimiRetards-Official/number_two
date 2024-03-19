f83b480138ef5a43690a0aaaf53f12c6b4047082
AdarshGrewal <adarshgrewal@gmail.com>
2024-03-20 05:01:38 +0530

    sm6225-common: Build IFAAService from source
    Change-Id: I804f0a424010cefaa3cc6f394c716b8b43302466

--- common.mk
+++ common.mk
@@ -279,6 +279,10 @@ PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/keylayout/,$(TARGET_COPY_OUT_VENDOR)/usr/keylayout)
 endif
 
+# IFAA
+PRODUCT_PACKAGES += \
+    IFAAService
+
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
